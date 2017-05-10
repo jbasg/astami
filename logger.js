f1bb0eb97bd6b18c9dcd913db31b77feef26b474
Jesus Arribas Gomez <jbas@hotmail.es>
2017-05-10 11:17:55 +0200

    Fix log_level

--- logger.js
+++ logger.js
@@ -5,7 +5,7 @@ module.exports = {
     color : { info : "\x1b[32m", error : "\x1b[31m", warn : "\x1b[33m", debug : "\x1b[34m" ,trace: "\x1b[36m" } ,
     levels : ['trace','debug','info','warn','error'],
     level :0 ,
-    set_level : function(level) { level = level },
+    set_level : function(lv) { level = lv },
 
     info : function(data){  this.write('info',Array.from(arguments)); },
     error  : function(data){  this.write('error',Array.from(arguments)); } ,
@@ -13,7 +13,7 @@ module.exports = {
     debug : function(data){ this.write('debug',Array.from(arguments)); },
     trace : function(data){ this.write('trace',Array.from(arguments)); },
     write : function(tipo,data){
-      if (this.levels.indexOf(tipo) >= this.level)
+      if (this.levels.indexOf(tipo) <= this.level)
       {
         console.log(this.color[tipo],`[ ${new Date().toISOString() } ][${tipo.toUpperCase()}]`,data);
       }
